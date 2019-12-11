696bd5b3add452d06ab73cf74f3ed4986a8e1eb2
Abdulla Mian <miana@claycorp.com>
2019-12-11 09:56:12 -0600

    default project

---
+++ BookStoreTest/BookStoreTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BookStoreExercism;
+using NUnit.Framework;
+
+namespace BookStoreTest
+{
+    public class BookStoreTests
+    {
+        [Test]
+        public void CreateBookStore()
+        {
+            BookStore bookStore = new BookStore();
+        }
+    }
+}
