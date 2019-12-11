5a518ace78bc17e1e891fb3afe724c8ba3f6d9fa
Abdulla Mian <miana@claycorp.com>
2019-12-11 11:14:54 -0600

    created discount and costOfBook method

--- BookStore/BookStore.cs
+++ BookStore/BookStore.cs
@@ -11,24 +11,45 @@ namespace BookStoreExercism
         {
             if (CheckIfBasketHasDistinctBooksOrIsEmpty(basket))
             {
-                return basket.Length * 8;
+                return basket.Length * 8 * 1.00;
             }
             else if (basket.Length == 4)
             {
-                return (basket.Length * 8) * 0.8;
+                return basket.Length * CostOfBook() * Discount(basket.Length);
             }
-            else if (basket.Length == 2)
+            else if (basket.Length == 3)
             {
-                return (basket.Length * 8) * 0.9;
+                return basket.Length * CostOfBook() * Discount(basket.Length);
             }
             else
             {
-                return (basket.Length * 8) * 0.95;
+                return basket.Length * CostOfBook() * Discount(basket.Length);
             }
             
         }
 
-        private static bool CheckIfBasketHasDistinctBooksOrIsEmpty(int[] basket)
+        private double Discount(int numberOfBooks)
+        {
+            if (numberOfBooks == 4)
+            {
+                return .80;
+            }
+
+            if (numberOfBooks == 3)
+            {
+                return .90;
+            }
+
+            return 0.95;
+        }
+
+
+        private int CostOfBook()
+        {
+            return 8;
+        } 
+
+        private bool CheckIfBasketHasDistinctBooksOrIsEmpty(int[] basket)
         {
             return basket.ToList().Distinct().Count() == 1 || basket.Length == 0;
         }
